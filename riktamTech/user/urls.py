28af2a72a3f82036a2306e948d65b7f05877c62e
himanikhurana <himanikhurana999@gmail.com>
2023-10-01 21:42:56 +0530

    Implement Admin APIs to manage (create and edit users) - Update urls.py

--- riktamTech/user/urls.py
+++ riktamTech/user/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('login', views.loginRequest),
     path('logout', views.logoutRequest),
     path('getUsers', views.getUsers),
+    path('createUser', views.create_user),
+    path('editUserDetails', views.edit_user)
 ]
